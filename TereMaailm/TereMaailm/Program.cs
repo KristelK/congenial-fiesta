46a1a9f18929c40b514f42387e54e1518f87b1ad
KristelK <38253820+KristelK@users.noreply.github.com>
2018-04-10 18:04:00 +0300

    Add something nice

--- TereMaailm/TereMaailm/Program.cs
+++ TereMaailm/TereMaailm/Program.cs
@@ -11,6 +11,7 @@ namespace TereMaailm
         static void Main(string[] args)
         {
             Console.WriteLine("Tere maailm!");
+            Console.WriteLine("Meeldiv tutvuda!");
         }
     }
 }
